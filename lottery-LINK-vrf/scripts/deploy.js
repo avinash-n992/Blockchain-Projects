99064b07f719ecc0c0fde9e3a3bf69f7d03090fb
Avin992 <avinash.nair6@gmail.com>
2022-02-04 19:34:15 +0530

    Changes in configs, deploy and secrets file

--- lottery-LINK-vrf/scripts/deploy.js
+++ lottery-LINK-vrf/scripts/deploy.js
@@ -3,12 +3,12 @@ async function main() {
   
     console.log("Deploying contracts with the account:", deployer.address);
   
-    // console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance:", (await deployer.getBalance()).toString());
   
-    // const Token = await ethers.getContractFactory("Token");
-    // const token = await Token.deploy();
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
   
-    // console.log("Token address:", token.address);
+    console.log("Token address:", token.address);
   }
   
   main()
